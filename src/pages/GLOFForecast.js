84ae9081107b6ac1297c52c07a2e0037a6619f66
codefean <emailfean001@gmail.com>
2025-07-30 11:06:48 +0200

    forecast

--- src/pages/GLOFForecast.js
+++ src/pages/GLOFForecast.js
@@ -4,6 +4,15 @@ import "./GLOFForecast.css";
 const GLOFForecast = () => {
   return (
     <div className="about-forecast-card">
+      <h2>Flood Forecasting (coming in 2026)</h2>
+      <p>
+        The Alaska GLOF Dashboard provides a comprehensive overview of glacial lake outburst flood (GLOF) events in Alaska. 
+        This platform is designed to help researchers, emergency responders, and the public understand the risks associated with GLOFs and access critical data for decision-making.
+      </p>
+      <p>
+        The dashboard includes data on glacial lakes, historical flood events, and (in the future) predictive models to forecast potential GLOF occurrences. 
+        It serves as a valuable resource for monitoring glacial activity and assessing the impact of climate change on glacial systems.
+      </p>
     </div>
   );
 };
